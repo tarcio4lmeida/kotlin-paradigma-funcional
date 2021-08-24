9d90a609835828148e51db365bb77e89f3b5e617
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-24 18:18:28 -0300

    tipo funcao com parametros e retorno

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,26 +9,27 @@ fun main (){
         println("Executa funcao anonima")
     }
 
+    println(minhaFuncaoAnonima)
+
+    testaTipoReferencia()
+    testaTipoReferenciaClasse()
+
+
 }
 
 fun testaTipoReferenciaClasse() {
-    val minhaFuncaoClasse: () -> Unit = Teste()
-    println(minhaFuncaoClasse())
+    val minhaFuncaoClasse: (Int, Int) -> Int = Teste()
+    println(minhaFuncaoClasse(20,5))
 }
 
-fun testaTipoReferencia() {
-    val minhaFuncao: () -> Unit = ::teste
-    minhaFuncao()
+fun testaTipoReferencia()  {
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 10))
 }
 
-fun teste(){
-    println("executa teste")
-}
+fun soma(a: Int, b: Int): Int = a + b
 
-class Teste : () -> Unit {
-
-    override fun invoke() {
-        println("executa invoke do teste")
-    }
+class Teste : (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int  = p1 + p2
 
 }
