e58231c091890a013f0104752ad48cb5cd11ccca
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-08-24 22:56:21 -0300

    Explorando implementacoes com let

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,52 +1,25 @@
 fun main() {
-    val minhaFuncaoLambda = {
-        println("Executa como lambda")
-    }
 
-    println(minhaFuncaoLambda)
+    val endereco = Endereco(
+        logradouro = "Rua Vergueiro",
+        numero = 3184
+    )
 
-    val minhaFuncaoAnonima: () -> Unit = fun() {
-        println("Executa funcao anonima")
-    }
+    val enderecoEmMaiusculo: String = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
 
-    println(minhaFuncaoAnonima)
+    println(enderecoEmMaiusculo)
 
-    testaTipoReferencia()
-    testaTipoReferenciaClasse()
+   Endereco(
+        logradouro = "Rua Vergueiro",
+        numero = 3184
+    ).let{
+        endereco -> "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    }.let(::println)
 
-    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
-        if (salario > 1000.0) {
-            return@lambda salario + 50
-        }
-        return@lambda salario + 100.0
-    }
-
-    println(calculaBonificacao(1000.0))
-
-    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
-        if (salario > 1000.0) {
-            return salario + 50
-        }
-        return salario + 100.0
-    }
-
-    println(calculaBonificacaoAnonima(1000.0))
 
 }
 
-fun testaTipoReferenciaClasse() {
-    val minhaFuncaoClasse: (Int, Int) -> Int = Teste()
-    println(minhaFuncaoClasse(20, 5))
-}
 
-fun testaTipoReferencia() {
-    val minhaFuncao: (Int, Int) -> Int = ::soma
-    println(minhaFuncao(5, 10))
-}
-
-fun soma(a: Int, b: Int): Int = a + b
-
-class Teste : (Int, Int) -> Int {
-    override fun invoke(p1: Int, p2: Int): Int = p1 + p2
-
-}
+class Endereco(
+    var logradouro: String,
+    var numero: Int)
---
+++ src/main/kotlin/tetaTipoFuncao.kt
@@ -0,0 +1,49 @@
+fun tetaFunfaoAnonima() {
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa funcao anonima")
+    }
+
+    println(minhaFuncaoAnonima)
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) {
+            return salario + 50
+        }
+        return salario + 100.0
+    }
+
+    println(calculaBonificacaoAnonima(1000.0))
+}
+
+fun testaFuncaoLambda() {
+    val minhaFuncaoLambda = {
+        println("Executa como lambda")
+    }
+
+    println(minhaFuncaoLambda)
+    val calculaBonificacao: (salario: Double) -> Double = lambda@{ salario ->
+        if (salario > 1000.0) {
+            return@lambda salario + 50
+        }
+        return@lambda salario + 100.0
+    }
+
+    println(calculaBonificacao(1000.0))
+}
+
+fun testaTipoReferenciaClasse() {
+    val minhaFuncaoClasse: (Int, Int) -> Int = Teste()
+    println(minhaFuncaoClasse(20, 5))
+}
+
+fun testaTipoReferencia() {
+    val minhaFuncao: (Int, Int) -> Int = ::soma
+    println(minhaFuncao(5, 10))
+}
+
+fun soma(a: Int, b: Int): Int = a + b
+
+class Teste : (Int, Int) -> Int {
+    override fun invoke(p1: Int, p2: Int): Int = p1 + p2
+
+}
